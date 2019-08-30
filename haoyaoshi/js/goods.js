8a36c4b699d0f173fe864b90d91f56d7b29e164e
wangx <wangxin@136.com>
2019-08-30 11:26:40 +0800

    liebiaoye

--- haoyaoshi/js/goods.js
+++ haoyaoshi/js/goods.js
@@ -1,12 +1,248 @@
 $(function() {
+    let ipage = 1;
+    let num = 20;
+    let type = 'normal';
     $.ajax({
         type: "post",
         url: "../php/goods.php",
-        // data: "data",
-        dataType: "json",
+        data: {
+            page: ipage,
+            num: num,
+            type: type
+        },
         success: function(response) {
-            console.log(response);
+
+            bb(response);
+            cc()
 
         }
     });
+
+
+    function cc() {
+
+        $('.fen').on('click', 'p', function() {
+            // console.log($(this).index());
+            let index = $(this).index();
+            console.log(index);
+
+            $(this).addClass("active").siblings().removeClass("active");
+            ipage = index + 1;
+            // // console.log(ipage);
+
+            $.ajax({
+                type: "post",
+                url: "../php/goods.php",
+                data: {
+                    page: ipage,
+                    num: num,
+                    type: type,
+                    order: order,
+                    a2: a2,
+                    a5: a5,
+                    a6: a6,
+                },
+
+                success: function(response) {
+                    // console.log(arr)
+                    var arr = JSON.parse(response);
+                    let html = arr.data.map(arr => {
+                        // console.log(arr);
+                        return `<li ${arr.index}>
+                <div class="tu">
+                    <img src=${arr.img1} alt="" class="img1">
+                </div>
+                <div class="name">${arr.name}</div>
+                <div class="black">${arr.black}</div>
+                <div class="store">${arr.store}</div>
+                <div class="des">${arr.des}</div>
+                <div class="price">
+                    <span class="now_price">￥${arr.now_price}</span>
+                    <span class="old_price">¥${arr.old_price}</span>
+                </div>
+                <div class="btn">
+                    <img src=${arr.img2} alt="" class="img2">
+                </div>
+            </li>`
+                    }).join('');
+                    $('.list1').html(html);
+                }
+            })
+        })
+
+    }
+
+    function bb(response) {
+
+        var arr = JSON.parse(response);
+        // console.log(arr);
+
+        let html = arr.data.map(arr => {
+            // console.log(arr);
+            return `<li ${arr.index}>
+            <div class="tu">
+                <img src=${arr.img1} alt="" class="img1">
+            </div>
+            <div class="name">${arr.name}</div>
+            <div class="black">${arr.black}</div>
+            <div class="store">${arr.store}</div>
+            <div class="des">${arr.des}</div>
+            <div class="price">
+                <span class="now_price">￥${arr.now_price}</span>
+                <span class="old_price">¥${arr.old_price}</span>
+            </div>
+            <div class="btn">
+                <img src=${arr.img2} alt="" class="img2">
+            </div>
+        </li>`
+        }).join('');
+        $('.list1').html(html);
+        let tol = Math.ceil(arr.tatol / arr.num);
+        // console.log(tol);
+        let fenye = '';
+        for (let i = 0; i < tol; i++) {
+            fenye += `<p>${i + 1}</p>`;
+        }
+        $('.fen').html(fenye);
+        $('.fen ').children().eq(ipage - 1).addClass("active");
+    };
+
+    var order = '';
+    //处理高到低排序
+    isok = true;
+    $('.paix').click(function() {
+        // console.log(1)
+        isok = !isok;
+        if (isok) {
+            order = 'desc';
+
+        } else {
+            order = 'asc';
+        }
+        type = 'paixu';
+
+        $.ajax({
+            type: "post",
+            url: "../php/goods.php",
+            data: {
+                order: order,
+                page: ipage,
+                num: num,
+                type: type
+            },
+            // dataType: "json",
+            success: function(response) {
+                bb(response);
+                cc()
+
+
+            }
+        });
+
+    })
+    var a2 = '';
+    $('.wx2  li').click(function() {
+
+            type = 'search';
+            a2 = $.trim($(this).text());
+            // console.log(a2);
+            $.ajax({
+                type: "post",
+                url: "../php/goods.php",
+                // dataType: "json",
+                data: {
+                    a2: a2,
+                    page: ipage,
+                    num: num,
+                    type: type
+                },
+                success: function(response) {
+
+                    bb(response);
+                    cc()
+                }
+            });
+        })
+        //处理区间查询
+    var a5 = '';
+    var a6 = '';
+    $('.wx3 li').click(function() {
+        type = 'qujian';
+        let w1 = $(this).text().split('元');
+        let w2 = w1[0].split('-');
+        // let w3 = $.trim(w2.split(','));
+        a5 = $.trim(w2[0]);
+        a6 = w2[1];
+        // console.log(a5);
+        $.ajax({
+            type: "post",
+            url: "../php/goods.php",
+            data: {
+                a5: a5,
+                a6: a6,
+                page: ipage,
+                num: num,
+                type: type
+            },
+            success: function(response) {
+
+                // console.log(response);
+                bb(response);
+                cc()
+            }
+        });
+    })
+    $('.b1').click(function() {
+        let index = $(this).index();
+        $(this).addClass("active").siblings().removeClass("active");
+        ipage = index + 1;
+        $.ajax({
+            type: "post",
+            url: "../php/goods.php",
+            data: {
+                a5: a5,
+                a6: a6,
+                page: ipage,
+                num: num,
+                type: type
+            },
+            success: function(response) {
+
+                // console.log(response);
+                bb(response);
+                cc()
+            }
+        });
+        // console.log(index)
+    })
+    $('.b4').click(function() {
+        console.log(1)
+            // $.ajax({
+            //     type: "post",
+            //     url: "../php/goods.php",
+            //     data: {
+            //         a5: a5,
+            //         a6: a6,
+            //         page: ipage,
+            //         num: num,
+            //         type: type
+            //     },
+            //     success: function(response) {
+
+        //         // console.log(response);
+        //         bb(response);
+        //         cc()
+        //     }
+        // });
+        // console.log(index)
+
+    })
+
+    // $('.z1').on('click', 'li', function() {
+    //     let a = $(this).attr('data-id');
+    //     console.log(a);
+    //     window.location.href = `http://127.0.0.1/MAlin.code/zuoye/html/xiangqing.html?id=${a}`;
+    // })
+
+
 })
--- haoyaoshi/php/goods.php
+++ haoyaoshi/php/goods.php
@@ -3,44 +3,56 @@ $db = mysqli_connect("127.0.0.1","root","","haoyaoshi");//连接数据库
 mysqli_query($db,"set names 'utf8'"); 
 mysqli_query($db,"set character set 'utf8'");//处理乱码
 //导入数据库
-$data = file_get_contents("../json/goods.json");
-$data = json_decode($data,true);
-for($i=0;$i<count($data);$i++){
-$img1 = $data[$i]["img1"];
-$name = $data[$i]["name"];
-$black = $data[$i]["black"];
-$store = $data[$i]["store"];
-$des = $data[$i]["des"];
-$now_price = $data[$i]["now_price"];
-$old_price = $data[$i]["old_price"];
-$img2 = $data[$i]["img2"];
-$sql = "INSERT INTO `goods`(`img1`,`name`,`black`,`store`,`des`,`now_price`,`old_price`,`img2`) 
-VALUES ('$img1','$name','$black','$store','$des','$now_price','$old_price','$img2')";
- mysqli_query($db,$sql);
-}
+// $data = file_get_contents("../json/goodlist.json");
+// $data = json_decode($data,true);
+// for($i=0;$i<count($data);$i++){
+// $img = $data[$i]["img"];
+// $price = $data[$i]["price"];
+// $style = $data[$i]["style"];
+// $title = $data[$i]["title"];
+// $sql = "INSERT INTO `taobao`(`img`,`price`,`style`,`title`) VALUES ('$img','$price','$style','$title')";
+//  mysqli_query($db,$sql);
+// }
 //处理数据库
 // $order =$_REQUEST['order'];
-// $page = isset($_REQUEST['page']) ? $_REQUEST['page'] : '1';
-// $num = isset($_REQUEST['num']) ? $_REQUEST['num'] : '10';
-// $index = ($page -1)*$num;
-// // var_dump($num);
-// $get = "SELECT * FROM taobao limit $index,$num ";
-// $sql='SELECT * FROM taobao ';
+$page = isset($_REQUEST['page']) ? $_REQUEST['page'] : '1';
+$num = isset($_REQUEST['num']) ? $_REQUEST['num'] : '10';
+$index = ($page -1)*$num;
+$a2 =isset($_REQUEST['a2'])?$_REQUEST['a2']:'';
+$a5 = isset($_REQUEST['a5'])?$_REQUEST['a5']:0;
+$a6 = isset($_REQUEST['a6'])?$_REQUEST['a6']:999999;
+$order = isset($_REQUEST['order']) ? $_REQUEST['order'] : '';
+$type =isset($_REQUEST['type']) ? $_REQUEST['type'] : 'normal';
+if ($type=='normal'){
+    $get = "SELECT * FROM goods limit $index,$num ";
+    $sql='SELECT * FROM goods ';
+}elseif ($type=='paixu') {
+    $get = "SELECT * FROM goods ORDER BY now_price $order limit $index,$num";
+    $sql = "SELECT * FROM goods ORDER BY now_price $order";
+}elseif($type=='search') {
+    $get = "SELECT * FROM goods WHERE `name` like '%$a2%' limit $index,$num";
+    $sql = "SELECT * FROM goods WHERE `name` like '%$a2%'";
+}elseif($type=='qujian') { 
+    $get = "SELECT * FROM goods WHERE now_price BETWEEN $a5 AND $a6 limit $index,$num";
+    $sql = "SELECT * FROM goods WHERE now_price BETWEEN $a5 AND $a6";
+}
 
-// //  $get = "SELECT * FROM taobao ORDER BY price $order";
-// //执行语句
+
+//  $get = "SELECT * FROM taobao ORDER BY price $order";
+//执行语句
 
 
-// $result = mysqli_query($db,$get);
-// $result1 = mysqli_query($db,$sql);
-// //拿到数据
-// $arr=mysqli_fetch_all($result, MYSQLI_ASSOC);
-// $data =array(
-//      'tatol'=>$result1->num_rows,
-//     'data'=>$arr,
-//     'page'=>$page,
-//     'num'=>$num,
-// );
+$result = mysqli_query($db,$get);
+$result1 = mysqli_query($db,$sql);
+//拿到数据
+$arr=mysqli_fetch_all($result, MYSQLI_ASSOC);
+$data =array(
+    'tatol'=>$result1->num_rows,
+    'data'=>$arr,
+    'page'=>$page,
+    'num'=>$num,
+);
+
 
 //转化打印
 echo json_encode($data, true);
